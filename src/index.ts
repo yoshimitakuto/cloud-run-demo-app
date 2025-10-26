aa41f94959a8c3d108402396525d7f09f11ee80f
YoshimiTakuto <takuto.yoshimi@ncdc.co.jp>
2025-10-26 11:45:17 +0900

    port 3001

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.post("/sum/:a/:b", (c) => {
   return c.json(sum({ a: Number(params.a), b: Number(params.b) }));
 });
 
-const port = Number(process.env.PORT) || 8080;
+const port = 3001;
 
 serve({ fetch: app.fetch, port }, (info) => {
   console.log(`Server running on http://localhost:${info.port}`);
